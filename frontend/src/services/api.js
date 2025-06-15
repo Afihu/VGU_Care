28414a8d08994868c4a39235e9b803ea733c919f
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-06-15 19:18:57 +0700

    feat: api call for password retrieval procedure

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
-const API_BASE_URL = 'http://localhost:5001/api/login' //defin
+const API_BASE_URL = 'http://localhost:5001/api' //define base api url
 
 const api = {
     authService : async(email, password) => {
-        const response = await fetch(API_BASE_URL, {
+        const apiEndpoint = API_BASE_URL + '/login';
+        const response = await fetch(apiEndpoint, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password }),
@@ -10,6 +11,17 @@ const api = {
         
         return response; 
     },    
+
+    passwordRetrievalService : async(email) => {
+        const apiEndpoint = API_BASE_URL + '/password-retrieve';
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email }),
+        });
+        
+        return response; 
+    } 
 }
 
 export default api;
